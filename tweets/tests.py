eb41fc1256763887be29c6fa2d541bbda3bcac41
Jinxin Hu <hujinxin0607@gmail.com>
2021-06-24 15:50:31 -0700

    Build tweets create api, testing

--- tweets/tests.py
+++ tweets/tests.py
@@ -4,11 +4,12 @@ from tweets.models import Tweet
 from datetime import timedelta
 from utils.time_helpers import utc_now
 
+
 class TweetTests(TestCase):
 
     def test_hours_to_now(self):
-        Xiaojinxin = User.objects.create_user(username='Xiaojinxin')
-        tweet = Tweet.objects.create(user = Xiaojinxin, content = "This is my first day to post tweet!")
-        tweet.created_at = utc_now() - timedelta(hours = 10)
+        Jinxin = User.objects.create_user(username='Jinxin')
+        tweet = Tweet.objects.create(user=Jinxin, content='Jinxin Dafa Good!')
+        tweet.created_at = utc_now() - timedelta(hours=10)
         tweet.save()
-        self.assertEqual(tweet.hours_to_now(),10)
+        self.assertEqual(tweet.hours_to_now, 10)
